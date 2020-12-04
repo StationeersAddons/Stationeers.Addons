f7f7ba3372d98441be3b81e074e37744e51f46e9
Damian Korczowski <erdroy2@gmail.com>
2020-12-05 00:57:59 +0100

    Updated example mod

--- Source/ExampleMod/Scripts/ExampleMod.cs
+++ Source/ExampleMod/Scripts/ExampleMod.cs
@@ -1,6 +1,7 @@
 ﻿// Stationeers.Addons (c) 2018-2020 Damian 'Erdroy' Korczowski & Contributors
 
 using Stationeers.Addons;
+using Stationeers.Addons.API;
 using UnityEngine;
 
 namespace ExampleMod.Scripts
@@ -10,6 +11,15 @@ namespace ExampleMod.Scripts
         public void OnLoad()
         {
             Debug.Log("Example Mod: Hello, World!");
+
+            var monkey = BundleManager.GetAssetBundle("monkey").LoadAsset<Mesh>("monkey");
+
+            var basicModel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var basicModelTransform = basicModel.transform;
+            basicModel.GetComponent<MeshFilter>().sharedMesh = monkey;
+            basicModelTransform.position = Vector3.zero;
+            basicModelTransform.localScale = Vector3.one * 4.0f;
+            basicModelTransform.rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
         }
 
         public void OnUnload()
