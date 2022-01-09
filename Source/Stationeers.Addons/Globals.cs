a82a033d203f2989b4de91d53c77c399f0c13f5c
Damian Korczowski <erdroy2@gmail.com>
2022-01-09 12:39:50 +0100

    Bump up Addons version to 0.3.1

--- Source/Stationeers.Addons/Globals.cs
+++ Source/Stationeers.Addons/Globals.cs
@@ -2,7 +2,7 @@ namespace Stationeers.Addons
 {
     public static class Globals
     {
-        public const string Version = "v0.3.0";
+        public const string Version = "v0.3.1";
         public const string VersionFile = "https://raw.githubusercontent.com/Erdroy/Stationeers.Addons/master/VERSION";
     }
 }
