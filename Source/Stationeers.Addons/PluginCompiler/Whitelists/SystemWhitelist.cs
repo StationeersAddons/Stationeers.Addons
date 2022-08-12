c1a127a3e90cedb05f82e42d67dbb1a60dd14392
Damian Korczowski <erdroy2@gmail.com>
2022-08-12 20:53:06 +0200

    Added more mscorlib types and namespaces to the whitelist

--- Source/Stationeers.Addons/PluginCompiler/Whitelists/SystemWhitelist.cs
+++ Source/Stationeers.Addons/PluginCompiler/Whitelists/SystemWhitelist.cs
@@ -21,19 +21,67 @@ namespace Stationeers.Addons.PluginCompiler.Whitelists
                 typeof(ulong),
                 typeof(float),
                 typeof(double),
-                typeof(decimal)
+                typeof(decimal),
                 
-                // TODO: Add more
-            );
-            whitelist.BlacklistTypes();
+                // These IO types are safe.
+                typeof(System.IO.Path),
+                typeof(System.IO.Stream),
+                typeof(System.IO.TextWriter),
+                typeof(System.IO.TextReader),
+                typeof(System.IO.BinaryReader),
+                typeof(System.IO.BinaryWriter),
+                typeof(System.IO.MemoryStream), // Should be safe
 
+                // System.* types
+                typeof(System.DateTime),
+                typeof(System.TimeSpan),
+                typeof(System.Array),
+                typeof(System.IDisposable),
+                typeof(System.StringComparison),
+                typeof(System.Math),
+                typeof(System.Enum),
+                typeof(System.Nullable<>),
+                typeof(System.IEquatable<>),
+                typeof(System.IComparable),
+                typeof(System.IComparable<>),
+                typeof(System.BitConverter),
+                typeof(System.FlagsAttribute),
+                typeof(System.Random),
+                typeof(System.Convert),
+                
+                typeof(System.Guid),
+                typeof(System.Runtime.InteropServices.LayoutKind),
+                typeof(System.Runtime.InteropServices.StructLayoutAttribute),
+                typeof(System.Runtime.InteropServices.GuidAttribute),
+                typeof(System.SerializableAttribute),
+                typeof(System.ComponentModel.DefaultValueAttribute),
+
+                // TODO: System.Reflection.Assembly* attributes?
+                // Kinda needed, but we do not have Assembly.cs, we don't need this
+                
+                // System.* Exceptions
+                typeof(System.Exception),
+                typeof(System.DivideByZeroException),
+                typeof(System.InvalidCastException),
+                typeof(System.ArgumentException),
+                typeof(System.ArgumentNullException),
+                typeof(System.NullReferenceException),
+                typeof(System.InvalidOperationException),
+                typeof(System.NotSupportedException)
+            );
+            whitelist.BlacklistTypes(/* none */);
+            
             whitelist.WhitelistTypesNamespaces(
+                // We no more allow the System.* namespace, but instead define all the types that are allowed above
                 typeof(System.Collections.IEnumerator),
                 typeof(System.Collections.Generic.List<>),
                 typeof(System.Collections.Concurrent.Partitioner<>),
-                typeof(System.Linq.Enumerable),
+                typeof(System.Collections.Immutable.ImmutableArray),
                 typeof(System.Text.StringBuilder),
-                typeof(System.Array)
+                typeof(System.Text.RegularExpressions.Match),
+                typeof(System.Timers.Timer),
+                typeof(System.Linq.Enumerable),
+                typeof(System.Xml.XmlDocument)
             );
         }
     }
