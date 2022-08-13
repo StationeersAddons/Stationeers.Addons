bd6f665b36f362a066e5d78aed600f6448b9c789
Damian Korczowski <erdroy2@gmail.com>
2022-08-13 10:50:50 +0200

    Bumped up version

--- Source/Stationeers.Addons/Constants.cs
+++ Source/Stationeers.Addons/Constants.cs
@@ -6,7 +6,7 @@ namespace Stationeers.Addons
 {
     public static class Constants
     {
-        public const string Version = "v0.4.0-pre";
+        public const string Version = "v0.4.0";
         public const string VersionFile = "https://raw.githubusercontent.com/Erdroy/Stationeers.Addons/master/VERSION";
         public static DateTime SandboxIntroductionDate => new DateTime(2022, 12, 1); // TODO: Update this before release
     }
