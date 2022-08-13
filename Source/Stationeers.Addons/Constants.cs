2ddb85fc810cbcd559b1519c134a4d0479dbc544
Damian Korczowski <erdroy2@gmail.com>
2022-08-13 13:41:26 +0200

    Updated Sandbox Introduction date to 2022-11

--- Source/Stationeers.Addons/Constants.cs
+++ Source/Stationeers.Addons/Constants.cs
@@ -8,6 +8,6 @@ namespace Stationeers.Addons
     {
         public const string Version = "v0.4.0";
         public const string VersionFile = "https://raw.githubusercontent.com/Erdroy/Stationeers.Addons/master/VERSION";
-        public static DateTime SandboxIntroductionDate => new DateTime(2022, 12, 1); // TODO: Update this before release
+        public static DateTime SandboxIntroductionDate => new DateTime(2022, 11, 30);
     }
 }
